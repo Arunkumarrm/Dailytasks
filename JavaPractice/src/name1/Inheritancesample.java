b6729e37395621bb7cac71342d2c83fd372b81f6
Arunkumarrm <marun7m@gmail.com>
2016-11-29 15:40:15 +0530

    accessing

---
+++ JavaPractice/src/name1/Inheritancesample.java
@@ -0,0 +1,12 @@
+package name1;
+
+public class Inheritancesample {
+	int p;
+	public void hello(){
+		System.out.println("hello sunny");
+	}
+	public void hai(){
+		System.out.println("hai sunny");
+	}
+
+}
