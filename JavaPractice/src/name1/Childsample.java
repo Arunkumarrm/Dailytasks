e1b19e5f90ad25f223ddc9a599cccc009e2c862c
Arunkumarrm <marun7m@gmail.com>
2016-11-29 15:39:49 +0530

    using other class methods

---
+++ JavaPractice/src/name1/Childsample.java
@@ -0,0 +1,20 @@
+package name1;
+
+public class Childsample extends Inheritancesample {
+	public void childmethod(){
+		System.out.println("this is single inheritance");
+	}
+	public void hai(){
+		System.out.println("this is not from inheritance sample");
+	}
+    public static void main(String[] args){
+    	Childsample c =new Childsample();
+    	c.childmethod();
+    	c.hai();
+    	Inheritancesample i =new Inheritancesample();
+    	i.hello();
+    	
+    	//hhh
+    	
+    }
+}
